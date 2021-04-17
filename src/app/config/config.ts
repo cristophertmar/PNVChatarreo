d42897a852cf7cb32b613523ce8ed95a60a50f2d
aezum19c <aezum19c@gmail.com>
2021-04-16 21:42:06 -0500

    Corregir observaciones para PCO y PCH

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 // export const URL_SERVICIOS = 'https://localhost:44356/';
 // export const URL_SERVICIOS = 'http://localhost:901/';
 export const URL_SERVICIOS = 'http://190.117.112.155/PNVCH/';
-export const ID_ROL = 1275;
+export const ID_ROL = 1273;
